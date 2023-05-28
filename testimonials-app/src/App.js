285ff5255f056478591fa83aa8bb09bbd0e116d6
Catalina Arriagada <catalina.arriagada1@gmail.com>
2023-05-28 15:20:26 -0400

    dynamic list react

--- testimonials-app/src/App.js
+++ testimonials-app/src/App.js
@@ -1,41 +1,12 @@
 import './App.css';
-import Testimonial from './components/Testimonial';
+import Testimonial from './components/Testimonial.jsx';
 
 function App() {
   return (
     <div className="App">
       <div className='main-container'>
         <h1>This is what our students say about us</h1>
-        
-        <Testimonial 
-          name='Shawn Wang'
-          country='Singapur'
-          image='shawn'
-          altPic = 'Shawn'
-          charge='Software Engineer'
-          company='Amazon'
-          testimonial='"<b>Lorem ipsum</b> dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."'
-         />
-
-        <Testimonial 
-          name='Sarah Chima'
-          country='Nigeria'
-          image='sarah'
-          altPic = 'Sarah'
-          charge='Software Engineer'
-          company='ChatDesk'
-          testimonial='"<b>Lorem ipsum dolor sit amet, consectetur adipiscing elit</b>, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."'
-         />
-        
-        <Testimonial 
-          name='Emma Bostian'
-          country='Sweden'
-          image='emma'
-          altPic = 'Emma'
-          charge='Software Engineer'
-          company='Spotify'
-          testimonial='"<b>Lorem ipsum dolor sit amet</b>, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."'
-         />
+        <Testimonial/>
       </div>
     </div>
   );
